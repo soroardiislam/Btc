d3390bae735d271e04c2af2de96e776fbd615581
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-20 09:41:25 +0600

    update

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navbar } from '../Components/Shars/Navbar'
 import { Hero } from '../Components/Home/Hero'
 import Footer from '../Components/Shars/Footer'
@@ -10,15 +9,16 @@ import Collapse from '../Components/Home/Collapse'
 
 export const HomeLayout = () => {
   return (
-    <div className='max-w-325 mx-auto px-12.5'>
+    <div className=' mx-auto px-12.5'>
       <Navbar/>
       <Hero></Hero>
       <Marque></Marque>
       <Crypto></Crypto>
       <Platform></Platform>
-      <Footer></Footer>
       <Currency></Currency>
       <Collapse></Collapse>
+      <Footer></Footer>
+      
     </div>
   )
 }
