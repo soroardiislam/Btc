4e435e4203ca8886a03d6cbd44a76cf86be43514
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-23 21:13:03 +0600

    update

--- src/Components/Home/Marque.jsx
+++ src/Components/Home/Marque.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import marque from '../../assets/marque.png'
+import React from "react";
+import marque from "../../assets/marque.png";
 
 export const Marque = () => {
   return (
-    <div className='py-18'>
-      <p className='text-center text-white font-semibold text-lg mb-10'>Trusted by top  <span className='text-[#99E39E]'>crypto platforms</span></p>
+    <div className="py-18">
+      <p className="text-center text-white font-semibold text-lg mb-10">
+        Trusted by top <span className="text-[#99E39E]">crypto platforms</span>
+      </p>
       <div>
-        <img className='w-full' src={marque} alt="" />
+        <img className="w-full" src={marque} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
